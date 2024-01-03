a22516731690d386f8c9f014d932f0a92a1c1ca4
abhrajit2004 <abhrajitgupta.it2022@nsec.ac.in>
2024-01-03 17:33:33 +0530

    third commit

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiYWJocmFqaXRndXB0YSIsImEiOiJjbHF3Zno4dzYwMmp3MmtteHJkMWhkb3V6In0.wiASQJiI6y28GUr1h-ReBQ';
 
+// Default Place
 const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
-    center: [88.30976, 22.5050624],
-    zoom: 14
+    center: [-73.99209, 40.68933],
+    zoom: 9
 });
 
+// Shows user's current location
 
 navigator.geolocation.getCurrentPosition((position)=>{
   const userLocation = [position.coords.longitude, position.coords.latitude];
@@ -17,19 +19,20 @@ navigator.geolocation.getCurrentPosition((position)=>{
 })
 
 
+// Shows nearest places 
+
 document.getElementById("btn").addEventListener("click",(e)=>{
   e.preventDefault();
-  let searchInput = document.getElementById("search").value;
-  let search = fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchInput}.json?access_token=${mapboxgl.accessToken}`);
-  search.then((response)=>{
-    console.log(response.json());
-  }).then((data)=>{
+  let searchInput = document.getElementById("mySearch").value;
+  let search = fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchInput}.json?access_token=${mapboxgl.accessToken}&session_token=${mapboxgl.accessToken}`);
+  search.then(response=> response.json()).then(data=>{
     if(data.features.length > 0){
       let coordinates = data.features[0].center;
       map.flyTo({
         center: coordinates,
-        zoom: 14
+        zoom: 9
       });
+      new mapboxgl.Marker().setLngLat(coordinates).addTo(map);
     }
     else{
       alert('Place not found. Please try again.');
