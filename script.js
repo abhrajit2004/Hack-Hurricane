d9c10a750d732e825d8813e28bfb7077980edef0
abhrajit2004 <abhrajitgupta.it2022@nsec.ac.in>
2024-01-03 20:31:55 +0530

    added files

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiYWJocmFqaXRndXB0YSIsImEiOiJjbHF3Zno4dzYwMmp3MmtteHJkMWhkb3V6In0.wiASQJiI6y28GUr1h-ReBQ';
+let marker;
+let popup;
 
 // Default Place
 const map = new mapboxgl.Map({
@@ -20,14 +22,24 @@ navigator.geolocation.getCurrentPosition((position)=>{
 
 
 // Shows nearest places 
+function searchPOI(coordinates){
+  let category = 'landmark';
+  let url = fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${category}.json?proximity=${coordinates[0]},${coordinates[1]}&access_token=${mapboxgl.accessToken}&session_token=${mapboxgl.accessToken}`);
 
-let marker;
+  url.then((response)=>response.json()).then((data)=>{
+    data.features.forEach(feature => {
+      new mapboxgl.Marker({color: 'green'}).setLngLat(feature.geometry.coordinates).addTo(map);
+    });
+  })
+
+}
 
 document.getElementById("btn").addEventListener("click",(e)=>{
   e.preventDefault();
   let searchInput = document.getElementById("mySearch").value;
   let search = fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchInput}.json?access_token=${mapboxgl.accessToken}&session_token=${mapboxgl.accessToken}`);
   search.then(response=> response.json()).then(data=>{
+    console.log(data.features[0].text);
     if(data.features.length > 0){
       let newCoordinates = data.features[0].center;
       map.flyTo({
@@ -38,6 +50,9 @@ document.getElementById("btn").addEventListener("click",(e)=>{
         marker.remove();
       }
       marker = new mapboxgl.Marker({color: 'red'}).setLngLat(newCoordinates).addTo(map);
+
+      popup =  new mapboxgl.Popup({className : 'mapboxgl-popup-content'}).setText(data.features[0].text);
+      marker.setPopup(popup);
     }
     else{
       alert('Place not found. Please try again.');
